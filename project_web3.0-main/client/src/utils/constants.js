afc216e05d3e8fa9fa9f00d3a37ab5aa1332d6e8
Arnav Chhokra <arnavchhokra@gmail.com>
2024-03-06 19:11:21 +0530

    Commit

--- project_web3.0-main/client/src/utils/constants.js
+++ project_web3.0-main/client/src/utils/constants.js
@@ -1,4 +1,4 @@
 import abi from "./Transactions.json";
 
-export const contractAddress = "0xfCCF80344a668b72ac4Be23513F0E9E4a35C84fA";
+export const contractAddress = "0x0963b812bc03d159fda7268b66a9d5252acb566e";
 export const contractABI = abi.abi;
